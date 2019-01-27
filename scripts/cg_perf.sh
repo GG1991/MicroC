78ed2640be814d2cde2e8dd8e81b795e5ce88bf5
Guido Giuntoli <gagiuntoli@gmail.com>
2019-01-27 19:03:47 -0300

    Renaming files for being congruent with MicroPP.

--- scripts/cg_perf.sh
+++ scripts/cg_perf.sh
@@ -4,15 +4,17 @@ source ./vars.sh
 
 mkdir -p result
 
+export OMP_NUM_THREADS=1
+
 PATH="./result"
-EXEC="../build/test/test_solver_linear"
+EXEC="../build/test/test_cg"
 
 for pc in ${petsc_pc[@]}; do
 	for n in ${sizes[@]}; do
 		for a in ${factor_a[@]}; do
 
-			echo "${EXEC} $n -pc_type $pc"
-			${EXEC} $n $a -pc_type $pc -ksp_monitor_true_residual > "$PATH/out_${pc}_${n}_${a}.txt"
+			echo "${EXEC} $n $a -pc_type $pc"
+			${EXEC} $n $a -pc_type $pc -ksp_monitor > "$PATH/out_${pc}_${n}_${a}.txt"
 
 
 		done
